8bfcd645dd0b646f4c0615f83b9cb043466b138f
FabianTianyiNi <tianyiskyni@gmail.com>
2017-05-31 17:40:29 -0500

    begin with jade design

--- blog1/routes/index.js
+++ blog1/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { name: 'Fabian Tianyi NI', phone: '(469)-510-9278', email: 'tianyiskyni@gmail.com'});
 });
 
 module.exports = router;
